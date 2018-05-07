1c566248f78745de99e40b8947229ddc73aee1ce
Peter Ujvari <1up8192@gmail.com>
2018-05-08 00:39:30 +0200

    owner token cut limit 50%

--- contracts/TokenRegistry.sol
+++ contracts/TokenRegistry.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.4.18;
 import './TokenSale.sol';
 
 contract TokenRegistry {
+
     address public owner;
+    uint tokenCutPercent = 2;
+    uint etherCutPercent = 2;
         
     struct TokenData {
         string name;
@@ -26,8 +29,8 @@ contract TokenRegistry {
         owner = msg.sender;
     }
     
-    function addToken(uint256 _rate, address _wallet, uint256 _cap, string _name, string _symbol, string _info, uint _saleStart, uint _saleEnd) public onlyOwner {
-        Crowdsale sale = new TokenSale(_rate, _wallet, _cap, _name, _symbol, owner, _saleStart, _saleEnd);
+    function addToken(uint256 _rate, address _wallet, uint256 _cap, string _name, string _symbol, string _info, uint _saleStart, uint _saleEnd, uint _ownerTokenCutPercent) public onlyOwner {
+        Crowdsale sale = new TokenSale(_rate, _wallet, _cap, _name, _symbol, owner, _saleStart, _saleEnd, _ownerTokenCutPercent, tokenCutPercent, etherCutPercent);
         tokens.push(TokenData({name: _name, info: _info, tokenAddress:address(sale.token), saleAddress:address(sale), timeAdded:now}));
         newTokenSale(tokens.length - 1);
     }
@@ -35,6 +38,12 @@ contract TokenRegistry {
     function getToken(uint number) public view returns (string name, string info, address tokenAddress, address saleAddress, uint timeAdded) {
         TokenData token = tokens[number];
         return (token.name, token.info, token.tokenAddress, token.saleAddress, token.timeAdded);
+    }
+
+    function modifyCuts(uint newTokenCutPercent, uint newEtherCutPercent) onlyOwner{
+        require(newTokenCutPercent <=10 && newEtherCutPercent <=10);
+            uint tokenCutPercent = newTokenCutPercent;
+            uint etherCutPercent = newEtherCutPercent;
     }
 
 }
--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -7,15 +7,47 @@ import './Crowdsale.sol';
 contract TokenSale is Crowdsale {
 
   address serviceCreator;
+  uint serviceCreatorEtherCutPercent;
+  uint ownerTokenCutPercent;
+  uint serviceCreatorTokenCutPercent;
 
-  function TokenSale(uint256 _rate, address _wallet, uint256 _cap, string _name, string _symbol, address _serviceCreator, uint _saleStart, uint _saleEnd) Crowdsale(_saleStart, _saleEnd, _rate, _wallet, new CustomToken(_cap,  _name,  _symbol)) public {
-  serviceCreator = _serviceCreator;
+  function TokenSale(uint256 _rate, address _wallet, uint256 _cap, string _name, string _symbol, address _serviceCreator, uint _saleStart, uint _saleEnd, uint _ownerTokenCutPercent, uint _serviceCreatorTokenCutPercent, uint _serviceCreatorEtherCutPercent) Crowdsale(_saleStart, _saleEnd, _rate, _wallet, new CustomToken(_cap,  _name,  _symbol)) public {
+    require(_ownerTokenCutPercent <= 50);  
+    serviceCreator = _serviceCreator;
+    serviceCreatorEtherCutPercent = _serviceCreatorEtherCutPercent;
+    ownerTokenCutPercent = _ownerTokenCutPercent;
+    serviceCreatorTokenCutPercent = _serviceCreatorTokenCutPercent;
   }
 
     function forwardFunds() internal {
-        uint serviceCreatorCut = msg.value / 100 * 5;
+        uint serviceCreatorCut = msg.value / 100 * serviceCreatorEtherCutPercent;
         wallet.transfer(msg.value - serviceCreatorCut);
         serviceCreator.transfer(serviceCreatorCut);
     }
+
+    function buyTokens(address beneficiary) public payable {
+        require(beneficiary != address(0));
+        require(validPurchase());
+
+        uint256 weiAmount = msg.value;
+
+        // calculate token amount to be created
+        uint256 tokens = getTokenAmount(weiAmount);
+        uint ownerTokens = tokens / 100 * ownerTokenCutPercent;
+        uint serviceCreatorTokens = tokens / 100 * serviceCreatorTokenCutPercent;
+
+        uint buyerTokens = tokens - ownerTokens - serviceCreatorTokens;
+
+        // update state
+        weiRaised = weiRaised.add(weiAmount);
+
+        token.mint(beneficiary, buyerTokens);
+        token.mint(wallet, ownerTokens);
+        token.mint(serviceCreator, serviceCreatorTokens);
+
+        TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+        forwardFunds();
+  }
 
 }
